e40d3118632508c2715f6135adae590ea887ea42
EzhovIgor-prog <iezow@mail.ru>
2026-09-19 08:41:37 +0200

    Сделана п1.3.4

--- 133-led-button-usb/main.c
+++ 133-led-button-usb/main.c
@@ -21,6 +21,27 @@ void set_led(bool on)
     printf("led %s\n", on ? "on" : "off");
 }
 
+bool handle_command(int command, bool led)
+{
+    if (command == 'e')
+    {
+        led = true;
+        set_led(led);
+    }
+    else if (command == 'd')
+    {
+        led = false;
+        set_led(led);
+    }
+    else
+    {
+        printf("unknown command: %c\n", command);
+    }
+
+    return led;
+}
+
+
 
 int main()
 {
@@ -48,5 +69,14 @@ int main()
         }
 
         previous = current;
+
+        int command = getchar_timeout_us(0);
+
+        if (command == PICO_ERROR_TIMEOUT)
+        {
+            continue;
+        }
+
+        led = handle_command(command, led);
     }
 }
